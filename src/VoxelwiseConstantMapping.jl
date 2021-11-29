2a3fc5083507e5acab9d6e00a5b3cb459f9f31da
Wonseok Shin <wsshin97@gmail.com>
2021-11-29 14:58:17 -0500

    Use AbbreviatedTypes; force vxlbound entries to be VecFloat

--- src/VoxelwiseConstantMapping.jl
+++ src/VoxelwiseConstantMapping.jl
@@ -1,14 +1,15 @@
 module VoxelwiseConstantMapping
 
-using StaticArrays
+using AbbreviatedTypes
+
 export VoxelwiseConstant
 export bounds
 
-struct VoxelwiseConstant{K,AN<:AbstractArray{<:Number,K},VR<:AbstractVector{<:Real}}
+struct VoxelwiseConstant{K,AN<:AbsArrNumber{K}}
     val::AN  # scalar values in voxels
-    vxlbound::NTuple{K,VR}  # locations of faces of voxels
+    vxlbound::NTuple{K,VecFloat}  # locations of faces of voxels
 
-    function VoxelwiseConstant{K,AN,VR}(val, vxlbound) where {K,AN,VR}
+    function VoxelwiseConstant{K,AN}(val, vxlbound) where {K,AN}
         size(val) .+ 1 == length.(vxlbound) ||
             @error "length.(vxlbound) = $(length.(vxlbound)) should be greater by 1 in each dimension than size(val) = $(size(val))."
 
@@ -20,13 +21,14 @@ struct VoxelwiseConstant{K,AN<:AbstractArray{<:Number,K},VR<:AbstractVector{<:Re
      end
 end
 
-VoxelwiseConstant(val::AN, vxlbound::NTuple{K,VR}) where {K,AN<:AbstractArray{<:Number,K},VR<:AbstractVector{<:Real}} =
-    VoxelwiseConstant{K,AN,VR}(val, vxlbound)
+VoxelwiseConstant(val::AN, vxlbound::NTuple{K,AbsVecReal}) where {K,AN<:AbsArrNumber{K}} =
+    VoxelwiseConstant{K,AN}(val, vxlbound)
 
-bounds(vc::VoxelwiseConstant) = (SVector(minimum.(vc.vxlbound)), SVector(maximum.(vc.vxlbound)))
+bounds(vc::VoxelwiseConstant) = (SVec(minimum.(vc.vxlbound)), SVec(maximum.(vc.vxlbound)))
 
-(vc::VoxelwiseConstant{K})(pt::AbstractVector{<:Real}) where {K} = vc(SVector{K}(pt))
-function (vc::VoxelwiseConstant{K})(pt::SVector{K,<:Real}) where {K}
+(vc::VoxelwiseConstant{K})(pt::AbsVecReal) where {K} = vc(SFloat{K}(pt))
+
+function (vc::VoxelwiseConstant{K,AN})(pt::SReal{K}) where {K,AN<:AbsArrNumber{K}}
     vxlbound = vc.vxlbound
     for k = 1:K
         vxlbound[k][1] ≤ pt[k] ≤ vxlbound[k][end] || @error "pt[$k] = $(pt[k]) should be between vxlbound[$k][1] = $(vxlbound[k][1]) and vxlbound[k][end] = $(vxlbound[k][end])."
